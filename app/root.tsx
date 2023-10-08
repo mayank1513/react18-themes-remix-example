42027a96d2456d8cec6ae6b9dd55668c6a188825
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-10-08 17:46:00 +0530

    add react18-themes

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,14 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { ThemeSwitcher } from "react18-themes";
+import ColorSchemePreference from "./color-scheme-preference";
+import ThemeSelector from "./theme-selector";
+import DarkThemeSelector from "./dark-theme-selector";
+import LightThemeSelector from "./light-theme-selector";
+// import PageNavigator from "./page-navigator";
+import styles from "./page.module.css";
+import "./globals.css";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
@@ -23,7 +31,76 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <ThemeSwitcher />
+        <div className="container">
+          <main className={`${styles.main}`}>
+            <div className={styles.description}>
+              <a
+                href="https://github.com/mayank1513/react18-themes"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.logo}
+              >
+                <p>
+                  <code className={styles.code}>react18-themes</code>
+                </p>
+              </a>
+              <Outlet />
+              <div>
+                <a
+                  href="https://mayank-chaudhari.vercel.app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  By Mayank
+                </a>
+              </div>
+            </div>
+
+            <div className={styles.center}>
+              <div>
+                <h1>
+                  Build with <code>react18-themes</code>
+                </h1>
+                <p>Unleash the power of React Server Components!</p>
+              </div>
+            </div>
+            <div className={[styles.center, styles.prefs].join(" ")}>
+              <div>
+                <ColorSchemePreference />
+                <ThemeSelector />
+              </div>
+              <div>
+                <DarkThemeSelector />
+                <LightThemeSelector />
+              </div>
+            </div>
+            <div className={styles.cards}>
+              {/* <PageNavigator /> */}
+              <a
+                href="https://github.com/mayank1513/react18-themes"
+                className={styles.card}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h2>
+                  More Examples <span>-&gt;</span>
+                </h2>
+                <p>Explore more examples on official GitHub Repo.</p>
+              </a>
+            </div>
+          </main>
+        </div>
+        <footer>
+          with 💖 by{" "}
+          <a
+            href="https://mayank-chaudhari.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Mayank Chaudhari
+          </a>
+        </footer>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
